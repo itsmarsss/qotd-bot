49cd4e0df17262c017a9d851b47c3f71b6771935
itsmarsss <itsmarzzzzzz@gmail.com>
2022-08-20 21:02:58 -0400

    Fix permission exception

--- src/QOTDBot/ButtonListener.java
+++ src/QOTDBot/ButtonListener.java
@@ -5,13 +5,15 @@ import net.dv8tion.jda.api.events.interaction.ButtonClickEvent;
 
 public class ButtonListener extends ListenerAdapter {
 	public void onButtonClick(ButtonClickEvent event) {
-		if(event.getButton().getId().startsWith("delete-notif")) {
-			event.getMessage().delete().queue();
-			
-			String commandId = event.getButton().getId().replace("delete-notif-", "");
-			event.getChannel().retrieveMessageById(commandId).queue(message -> {
-				message.delete().queue();
-			});
-		}
+		try {
+			if(event.getButton().getId().startsWith("delete-notif")) {
+				event.getMessage().delete().queue();
+
+				String commandId = event.getButton().getId().replace("delete-notif-", "");
+				event.getChannel().retrieveMessageById(commandId).queue(message -> {
+					message.delete().queue();
+				});
+			}
+		}catch(Exception e) {}
 	}
 }
