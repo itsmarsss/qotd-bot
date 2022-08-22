0d0eb39f681cfc025e7e31cf15e6202853c76b1e
itsmarsss <itsmarzzzzzz@gmail.com>
2022-08-21 22:01:30 -0400

    Update help list

--- src/QOTDBot/CMD.java
+++ src/QOTDBot/CMD.java
@@ -612,7 +612,8 @@ public class CMD extends ListenerAdapter{
 						QOTDBot.config.getPrefix() + " unpause` - Unpause QOTD posting" + "\n`" + 
 						QOTDBot.config.getPrefix() + " prefix <prefix, no space>` - Change bot prefix" + "\n`" + 
 						QOTDBot.config.getPrefix() + " managerreview <true|false>` - Toggle QOTD manager review" + "\n`" + 
-						QOTDBot.config.getPrefix() + " reviewchannel <channel id>` - Set QOTD request channel", false)
+						QOTDBot.config.getPrefix() + " reviewchannel <channel id>` - Set QOTD request channel" + "\n`" + 
+						QOTDBot.config.getPrefix() + " info` - See bot info", false)
 				.addBlankField(true)
 				.addField("Admin commands",
 						"`" + QOTDBot.config.getPrefix() + " permrole <role id/'everyone'>` - Set QOTD permission role" + "\n`" + 
