22de570c5db5bc8ab3379820f56dd7455a941ab3
itsmarsss <88464166+itsmarsss@users.noreply.github.com>
2022-08-22 19:43:14 -0400

    Add write config.yml method

--- src/QOTDBot/QOTDBot.java
+++ src/QOTDBot/QOTDBot.java
@@ -45,7 +45,7 @@ public class QOTDBot {
 	private static LinkedList<Question> questions = new LinkedList<Question>();
 	private static boolean isPaused = false;
 
-	static final String version = "2.8.4";
+	static final String version = "2.8.5";
 	private static String parent;
 	private static final EnumSet<GatewayIntent> intent = EnumSet.of(GatewayIntent.GUILD_MESSAGES);
 	
@@ -230,6 +230,64 @@ public class QOTDBot {
 			if(!config.isValid()) {
 				return false;
 			}
+			config.setInitialized(true);
+			return true;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return false;
+		}
+	}
+	
+	static boolean writeConfigYML() {
+		String template = "# IMPORTANT - Mandatory fields:\r\n"
+				+ "# Input the bot's token here, this can be found in Developers Portal > Applications > [Bot Profile] > Bot > Token > [Copy]\r\n"
+				+ "botToken: %s\r\n"
+				+ "\r\n"
+				+ "# Turn on developers mode in Settings > Advanced > Developer Mode, right click your Discord Server and click on [Copy ID]\r\n"
+				+ "serverID: %s\r\n"
+				+ "\r\n"
+				+ "# Right click your QOTD channel and click on [Copy ID]\r\n"
+				+ "channelID: %s\r\n"
+				+ "\r\n"
+				+ "# Set a prefix here for the bot, this is what members use to use the bot (e.g. qotd help)\r\n"
+				+ "prefix: %s\r\n"
+				+ "\r\n"
+				+ "# Input number of minute(s) until another QOTD is sent ( 1 to 1440 [24 hours] )\r\n"
+				+ "interval: %s\r\n"
+				+ "\r\n"
+				+ "# Start time, this dictates what time of the day your QOTD will be sent (24 hours time, local server time)\r\n"
+				+ "# Hours\r\n"
+				+ "hour: %s\r\n"
+				+ "# Minutes\r\n"
+				+ "minute: %s\r\n"
+				+ "\r\n"
+				+ "# Not mandatory fields:\r\n"
+				+ "# Set a perm role, these members can add QOTDs (write everyone if everyone)\r\n"
+				+ "permRoleID: %s\r\n"
+				+ "\r\n"
+				+ "# Set a manager role, these members can manage QOTDs (write everyone if everyone)\r\n"
+				+ "managerRoleID: %s\r\n"
+				+ "\r\n"
+				+ "# QOTD submission review settings\r\n"
+				+ "# Set to true if you want QOTD submissions to go through bot manager review\r\n"
+				+ "managerReview: %s\r\n"
+				+ "# Right click your QOTD review channel and click on [Copy ID], this is where QOTD submissions are reviewed\r\n"
+				+ "reviewChannel: %s\r\n";
+		
+		String token = config.getBotToken();
+		String serverID = config.getServerID();
+		String channelID = config.getChannelID();
+		String prefix = config.getPrefix();
+		int interval = config.getInterval();
+		int hour = config.getHour();
+		int minute = config.getMinute();
+		String permRoleID = config.getPermRoleID();
+		String managerRoleID = config.getManagerRoleID();
+		boolean managerReview = config.getManagerReview();
+		String reviewChannel = config.getReviewChannel();
+		
+		try (FileWriter file = new FileWriter(parent + "/config.yml")) {
+			file.write(String.format(template, token, serverID, channelID, prefix, interval, hour, minute, permRoleID, managerRoleID, managerReview, reviewChannel));
 			return true;
 		} catch (Exception e) {
 			return false;
