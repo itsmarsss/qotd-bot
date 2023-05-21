ea85e2b310cdc1db83b9a20d64adcd9914aa1c2a
itsmarsss <itsmarzzzzzz@gmail.com>
2023-05-21 12:19:07 -0400

    Add settings and queue

--- app/src/main/resources/webassets/index.js
+++ app/src/main/resources/webassets/index.js
@@ -1,6 +1,21 @@
+const queue = document.getElementById('queue');
+const review = document.getElementById('review');
 
+const question_queue = document.getElementById('question-queue');
+const question_review = document.getElementById('question-review');
 
 function deleteQOTD(index) {
 
 }
 
+queue.addEventListener("click", function () {
+    question_queue.style.display = "block";
+    question_review.style.display = "none";
+});
+
+review.addEventListener("click", function () {
+    question_queue.style.display = "none";
+    question_review.style.display = "block";
+});
+
+queue.click();
