35a3bf3505773df335ea2f8add656df7260a965a
itsmarsss <itsmarzzzzzz@gmail.com>
2023-05-21 23:27:56 -0400

    Add list title

--- app/src/main/resources/webassets/index.js
+++ app/src/main/resources/webassets/index.js
@@ -4,6 +4,8 @@ const review = document.getElementById('review');
 const question_queue = document.getElementById('question-queue');
 const question_review = document.getElementById('question-review');
 
+const list_title_text = document.getElementById('list-title-text');
+
 function deleteQOTD(index) {
 
 }
@@ -11,11 +13,15 @@ function deleteQOTD(index) {
 queue.addEventListener("click", function () {
     question_queue.style.display = "block";
     question_review.style.display = "none";
+
+    list_title_text.innerHTML = "Queue:"
 });
 
 review.addEventListener("click", function () {
     question_queue.style.display = "none";
     question_review.style.display = "block";
+
+    list_title_text.innerHTML = "Review:"
 });
 
 queue.click();
