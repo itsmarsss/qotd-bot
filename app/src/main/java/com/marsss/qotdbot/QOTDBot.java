e23a4d2b01f40e45173a934e4f8f50117c45541a
itsmarsss <itsmarzzzzzz@gmail.com>
2023-07-04 09:22:18 -0400

    Version update

--- app/src/main/java/com/marsss/qotdbot/QOTDBot.java
+++ app/src/main/java/com/marsss/qotdbot/QOTDBot.java
@@ -40,7 +40,7 @@ public class QOTDBot {
     private static final LinkedList<String> uuids_review = new LinkedList<>();
     private static boolean isPaused = false;
 
-    static final String version = "4.3.9";
+    static final String version = "4.4.0";
     private static String parent;
     private static final EnumSet<GatewayIntent> intent = EnumSet.of(
             GatewayIntent.GUILD_MESSAGES,
